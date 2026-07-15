e641aaac5086cc6bb1d670170a487b8d6759de1c
Durdom55 <299125563+Durdom55@users.noreply.github.com>
2026-07-15 13:37:42 +0300

    New window

--- BetterZapret.py
+++ BetterZapret.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 import tkinter.messagebox
 import tkinter.filedialog
 import os
@@ -97,6 +98,28 @@ def PathCreate():
             path = ""
         print("error")
         
+def newWindow():
+    window = Toplevel()
+    window.title("List-general")
+    window.protocol("WM_DELETE_WINDOW", lambda: closeWindow(window))
+    window.iconbitmap(default="BetZaicon.ico")
+    window.attributes('-topmost', 1)
+    window.geometry("600x500+650+200")
+    window.resizable(False, False)
+    Label(window, text="Домены сайтов", font=("Bahnschrift Bold", 17)).grid(row=1, column=1)
+    textPath = Label(window, text=f"Актуальный путь:\n     ", font=("Bahnschrift", 15))
+    textPath.grid(row=2, column=1, padx=[10, 0])
+    PathAuto = ttk.Button(window, text="Определить путь автоматически")
+    PathAuto.grid(row=2, ipadx=1, ipady=10, padx=[20, 0], pady=10)
+    PathShow = ttk.Button(window, text="Определить путь вручную")
+    PathShow.grid(row=3, ipadx=10, ipady=10, padx=[20,0], pady=1)
+    window.grab_set()
+    
+
+def closeWindow(window):
+    window.grab_release()
+    window.destroy()
+        
 #---FRAME 1---
 frame1 = Frame(app)
 bg = Label(frame1, image=Bg1)
@@ -106,7 +129,7 @@ but = Button(frame1, bg="#070707", activebackground ="#070707", image=butOff_i, 
 but.place(x=129)
 pathbut = Button(frame1, relief=FLAT, bd=0, image=PathFlInv, activebackground="#070707", bg="#070707", width=62, height=36, command=PathCreate)
 pathbut.place(x=1, y=1)
-listbut = Button(frame1, relief=FLAT, bd=0, image=_listbutInv, activebackground="#070707", bg="#070707", width=60, height=62)
+listbut = Button(frame1, relief=FLAT, bd=0, image=_listbutInv, activebackground="#070707", bg="#070707", width=60, height=62, command=newWindow)
 listbut.place(x=1,y=55)
 
 #---ХОД ПРОГРАММЫ---
